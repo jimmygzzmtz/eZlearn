2943a0eab61683e96eee4a2b23440852aec294f6
Jaime González <jimmygzzmtz@gmail.com>
2019-10-06 23:32:09 +0100

    Updated Icon and Theming
    Also added install instructions.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {
+
+  }
+
+  async installPrompt(){
+    const alert = await this.alertController.create({
+      header: "Install",
+      message: "<ion-icon name='logo-apple'></ion-icon> Tap on <ion-icon name='share'></ion-icon>, then 'Add to Homescreen'.<br><br><ion-icon name='logo-android'></ion-icon> Tap on <ion-icon name='menu'></ion-icon>, then 'Add to Homescreen'.<br><br><ion-icon name='logo-chrome'></ion-icon> Click on <ion-icon name='add'></ion-icon> on the address bar.",
+      buttons: [
+        {
+            text: 'OK'
+        }
+    ]
+    });
+
+    await alert.present();
+  }
 
 }
